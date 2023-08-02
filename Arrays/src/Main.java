ecb4437554496e2b7563262929fdd8ab7989379c
ASUS <ASUS@192.168.11.14>
2023-08-02 17:50:21 -0500

    Updated Test 1

---
+++ Arrays/src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Random;
+
+public class Main {
+
+	public static void main(String[] args) {
+		long []arregloEnterosLargos = new long[20];
+		
+		Random r = new Random ();
+		
+		for (int i = 0; i < arregloEnterosLargos.length; i++) {
+			arregloEnterosLargos [i] = r.nextInt(100);
+		}
+		
+		for (int i = 0; i < arregloEnterosLargos.length; i++) {
+		  System.out.print(arregloEnterosLargos [i]+ " - ");
+		}
+		
+//		Arrays.
+	}
+}
