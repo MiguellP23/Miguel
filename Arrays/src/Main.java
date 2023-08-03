2993443a7b459438ccae6ed92164722b8a023d4c
Miguel <mapinedav@unbosque.edu.co>
2023-08-02 19:18:08 -0500

    Update test 3

--- Arrays/src/Main.java
+++ Arrays/src/Main.java
@@ -14,7 +14,9 @@ public class Main {
 		for (int i = 0; i < arregloEnterosLargos.length; i++) {
 		  System.out.print(arregloEnterosLargos [i]+ " - ");
 		}
-		System.out.println("Uwu");
+		System.out.println(" UwU ");
+		
+		System.out.println(" Testeo ");
 		
 //		Arrays.
 	}
